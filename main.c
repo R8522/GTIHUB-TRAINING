c76145b43cd6d1cb54f53099c97b548cddc1bc63
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2022-05-28 11:52:52 +0800

    Update main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+//documentation section
+/*rabiatuladawiah teusday, date 2-november-2021, tutorial 1*/
+
+//pre-processor section
+#include<stdio.h>
+
+//global variable section
+
+//main function section
+int main(){
+    //style 1
+    int zulaikaWeight = 50;
+    printf("this is zulaika weight %d", zulaikaWeight);
+
+    //style 2
+    int zulaikaHeight = 162;
+    printf("this is zulaika height %d", zulaikaHeight);
+}
